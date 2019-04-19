87785209601576c81c5e681ad919589e16d45331
jack-storytailor <storytelling.as.science@gmail.com>
2019-04-19 06:50:46 +0300

    0.0.13

--- client/lib/example-project/tsrc/index.js
+++ client/lib/example-project/tsrc/index.js
@@ -2,11 +2,15 @@ import * as story from '../story/index.sts';
 import * as __env from './environment';
 
 let contentContainer = document.getElementById('content');
-let storyContent = __env.getSerializer().serialize(story, '\n');
-let htmlStoryContent = `${storyContent.split('\n').map((str) => `<div> ${str} &nbsp;</div>`).join('')}`;
+let storyContent = __env.getSerializer().serialize(story, '\r\n');
+let htmlStoryContent = `${storyContent.split('\r\n').map((str) => {
+
+  if (!str || str === '') {
+    str = '&nbsp;'
+  }
+  return `<div class="content-line">${str}</div>`;
+}).join('')}`;
 
 if (contentContainer) {
   contentContainer.innerHTML = htmlStoryContent
 }
-
-console.log('story is ', story);
