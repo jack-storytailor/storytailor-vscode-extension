c7a47edffc45bdb4ccc19ea33f35e7ff091c826f
Jack Sydorenko <yevhens@superplay.co>
2023-11-28 01:24:48 +0200

    sync

--- readme/out/system.js
+++ readme/out/system.js
@@ -3,12 +3,16 @@ let __context = { __text: [] };
 let __serializer = __env.getSerializer();
 
 __context['hyperlink'] = function (урл, текст) {
+__context['	'];
 return `[${__serializer.serialize( текст, '\r\n' )}](${__serializer.serialize( урл, '\r\n' )})`;
 };
 __context['localPhoto'] = function (урл, текст, imgRoot) {
+__context['	'];
 if (!imgRoot) { 
-	imgRoot = `https:\//raw.githubusercontent.com/jack-storytailor/storytailor-vscode-extension/master/client/lib/images/`;
+	__context['		@imgRoot'] = `https:\//raw.githubusercontent.com/jack-storytailor/storytailor-vscode-extension/master/client/lib/images/`;
+	__context['	'];
  };
+__context['	'];
 return `![${__serializer.serialize( текст, '\r\n' )}](${__serializer.serialize( imgRoot, '\r\n' )}${__serializer.serialize( урл, '\r\n' )})`;
 };
 
